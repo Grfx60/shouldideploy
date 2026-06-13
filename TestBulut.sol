3d99bff0c549d97c65b85c1de4bc43cdc88b0eeb
Grfx60 <mavigrfx@hotmail.com>
2026-06-13 22:08:12 +0300

    Create TestBulut.sol

---
+++ TestBulut.sol
@@ -0,0 +1,7 @@
+contract AcilFon {
+    mapping(address => uint) public bakiyeler;
+    function paraCek() public {
+        msg.sender.call{value: bakiyeler[msg.sender]}("");
+        bakiyeler[msg.sender] = 0;
+    }
+}
