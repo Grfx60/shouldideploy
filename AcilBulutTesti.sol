250d1b3236e1e6c572d72a499947d8777779bc1c
Grfx60 <mavigrfx@hotmail.com>
2026-06-13 22:32:01 +0300

    Implement withdraw function in TestAkilliSozlesme
    Fix withdrawal function to reset balance after call.

--- AcilBulutTesti.sol
+++ AcilBulutTesti.sol
@@ -1,1 +1,8 @@
 
+contract TestAkilliSozlesme {
+    mapping(address => uint) balances;
+    function withdraw() public {
+        msg.sender.call{value: balances[msg.sender]}(""); // Hatalı Satır
+        balances[msg.sender] = 0;
+    }
+}
